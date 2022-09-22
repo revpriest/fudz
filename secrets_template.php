22fe94b44a40760a3aaa7223d6e794ae07c5971a
Pre <pre@dalliance.net>
2022-09-22 19:21:33 +0200

    Add template for secrets.php

---
+++ secrets_template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* There's a bunch of stuff you can change just by creating
+* a secrets.php file. Look at index.php. Loads of it. Here's
+* a few examples with none-real secrets. You can copy this
+* file to secrets.php and edit it if you want
+*/
+
+$hostname = "fuddyfuds.madeup";
+$homepath = "/fudz/";
+$allowedIps = ["1.2.3.4","localhost","127.0.0.1","8.8.8.8"];
+$whitelistPaths = [];
+
+
+$nitterInstances = [
+  "nitter.mine.madeup",
+];
